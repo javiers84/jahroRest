59831cd614d50c016c98e5ed586e862700eab7b6
javiers84 <makako84@gmail.com>
2022-10-14 21:14:38 -0300

    puesta en prod ftp

--- config/mail.js
+++ config/mail.js
@@ -1,40 +1,73 @@
 const { response } = require('express');
 const nodemailer = require('nodemailer');
 
-const createTrans = () => {
-    const transport = nodemailer.createTransport({
-        service: 'gmail',
-        // host: "smtp.gmail.com",
-        // port: 2525,
+
+module.exports = (formulario) => {
+    var transporter = nodemailer.createTransport({
+        service: gmail,
         auth: {
-            user: "makako84@gmail.com",
-            pass: "reyvaj++1984"
+            user: 'makako84@gmail.com',
+            pass: 'reyvaj++1984'
         }
     });
-    return transport;
-}
 
-const sendMail = async(req, res = response) => {
+    const mailOption = {
+        from: `${formulario.nombre} <${formulario.correo}>`,
+        to: 'info@jahro.com.ar',
+        subject: 'Consulta desde la AppWeb',
+        html: `
+            <strong>Nombre: </strong> ${formulario.nombre} <br/>
+            <strong>Apellido: </strong> ${formulario.apellido} <br/>
+            <strong>Telefono: </strong> ${formulario.telefono} <br/>
+            <strong>Mensaje: </strong> ${formulario.consulta} <br/>
+        `
+    };
 
-    const { nombre, apellido, correo, telefono, consulta } = req.body;
-    const transporter = createTrans();
-    const info = await transporter.sendMail({
-        from: `${correo}`,
-        to: 'j_javier_m@hotmail.com',
-        subject: 'Consulta desde la appWeb',
-        html: "<h1>Hola</h1><p>mi nombre es " + nombre + " " + apellido + " mi telefono es " + telefono + " mi mensaje es " + `${consulta}` + "</p>"
-    }).then(() => {
-        console.log('Message enviado')
-    }).catch((error) => {
-        console.log(error.response.body)
+    transporter.sendMail(mailOption, function(err, info) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(info);
+        }
     });
-
-    console.log('Message sent: %s', info.messageId);
-
-    return
 }
 
-exports.sendMail = () => sendMail;
+
+
+
+// const createTrans = () => {
+//     const transport = nodemailer.createTransport({
+//         service: 'gmail',
+//         // host: "smtp.gmail.com",
+//         // port: 2525,
+//         auth: {
+//             user: "makako84@gmail.com",
+//             pass: "reyvaj++1984"
+//         }
+//     });
+//     return transport;
+// }
+
+// const sendMail = async(req, res = response) => {
+
+//     const { nombre, apellido, correo, telefono, consulta } = req.body;
+//     const transporter = createTrans();
+//     const info = await transporter.sendMail({
+//         from: `${correo}`,
+//         to: 'j_javier_m@hotmail.com',
+//         subject: 'Consulta desde la appWeb',
+//         html: "<h1>Hola</h1><p>mi nombre es " + nombre + " " + apellido + " mi telefono es " + telefono + " mi mensaje es " + `${consulta}` + "</p>"
+//     }).then(() => {
+//         console.log('Message enviado')
+//     }).catch((error) => {
+//         console.log(error.response.body)
+//     });
+
+//     console.log('Message sent: %s', info.messageId);
+
+//     return
+// }
+// exports.sendMail = () => sendMail;
 
 
 
@@ -75,29 +108,6 @@ exports.sendMail = () => sendMail;
 //     console.log('Message sent: %s', info.messageId);
 
 //     return
-// }
-
-// exports.sendMail = () => sendMail;
-
-
-// const sendMail = async() => {
-//     const sgMail = require('@sendgrid/mail')
-//     sgMail.setApiKey(process.env.SENDGRID_API_KEY)
-//     const msg = {
-//         to: 'j_javier_m@hotmail.com', // Change to your recipient
-//         from: 'makako84@gmail.com', // Change to your verified sender
-//         subject: 'Enviando correo con SendGrid',
-//         text: 'Prueba de envio atraves de Node js',
-//         html: '<strong>Prueba de envio atraves de Node js</strong>',
-//     }
-//     sgMail
-//         .send(msg)
-//         .then(() => {
-//             console.log('Mail enviado correctamente')
-//         })
-//         .catch((error) => {
-//             console.error(error)
-//         })
 // }
 
 // exports.sendMail = () => sendMail;
--- routes/mail.js
+++ routes/mail.js
@@ -1,10 +1,16 @@
 const { Router } = require('express');
+// const { email } = require('../config/mail');
 const email = require('../config/mail')
 
 const router = Router();
 
-router.post('/enviar',
-    email.sendMail()
-);
+// router.post('/enviar',
+//     email.sendMail()
+// );
+
+router.post('/enviar', (req, res) => {
+    email(req.body);
+    res.status(200);
+});
 
 module.exports = router;
